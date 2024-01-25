d63956350351ba171e2d7280c9118573ed59f50c
middleclicker <60602265+middleclicker@users.noreply.github.com>
2024-01-26 00:37:52 +0800

    Make some small changes

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     private static int kFactor = 32,
             cFactor = 400,
-            initialElo = 1000,
             minimumElo = 100,
+            initialElo = 1000,
             currentElo = initialElo;
 
     public static void main(String[] args) {
@@ -15,10 +15,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String input = scanner.nextLine(); //  "Win 1000" or "Lose 1000"
+            String input = scanner.nextLine();
             if (input.equalsIgnoreCase("exit")) {
                 break;
             }
+            // e.g. Win 1000 or Lose 1000
             opponentElo = Integer.parseInt(input.split(" ")[1]);
             matchStatus = status(input.split(" ")[0]);
 
@@ -26,25 +27,15 @@ public class Main {
         }
     }
 
-    public static int status(String status) {
-        if (status.equalsIgnoreCase("win")) {
-            return 2;
-        } else if (status.equalsIgnoreCase("lose")) {
-            return 0;
-        } else { // Draw
-            return 1;
-        }
-    }
-
-    public static void elo(int personalElo, int opponentElo, int matchStatus) {
-        double expected_A = 1 / (1 + Math.pow(10d, (double)(opponentElo-personalElo)/cFactor));
-        float actual_A = 0f;
+    private static void elo(int personalElo, int opponentElo, int matchStatus) {
+        double expected_A = 1 / (1 + Math.pow(10d, (double)(opponentElo-personalElo)/cFactor)); // Expected match result
+        float actual_A = 0f; // Actual match result
         if (matchStatus == 0) {
-            actual_A += 0f;
+            actual_A = 0f;
         } else if (matchStatus == 2) {
-            actual_A += 1f;
+            actual_A = 1f;
         } else {
-            actual_A += 0.5f;
+            actual_A = 0.5f;
         }
 
         currentElo = (int) Math.floor(personalElo + kFactor*(actual_A - expected_A));
@@ -53,10 +44,21 @@ public class Main {
             currentElo = 100;
         }
 
-        if (currentElo >= personalElo) {
-            System.out.println("Current Elo: " + currentElo + " (+" + (currentElo-personalElo) + ")"); // Current Elo: 1016 (+16)
+        if (currentElo >= personalElo) { // Current Elo: 1016 (+16)
+            System.out.println("Current Elo: " + currentElo + " (+" + (currentElo-personalElo) + ")");
         } else {
-            System.out.println("Current Elo: " + currentElo + " (-" + (personalElo-currentElo) + ")"); // Current Elo: 900 (-100)
+            System.out.println("Current Elo: " + currentElo + " (-" + (personalElo-currentElo) + ")");
         }
     }
+
+    private static int status(String status) {
+        if (status.equalsIgnoreCase("win")) {
+            return 2;
+        }
+        if (status.equalsIgnoreCase("lose")) {
+            return 0;
+        }
+        return 1; // Draw
+    }
+
 }
